42d7883c39822c38e83771474a80908b7687c71b
mbrad26 <mbrad26@gmail.com>
2020-04-01 13:26:50 +0000

    Added test for numbers multiple of 5

---
+++ fizzbuzz-alt/lib/fizzbuzz-alt.rb
@@ -0,0 +1,11 @@
+class Integer
+  
+  def fizzbuzz
+    if self % 3 == 0
+      'fizz'
+    elsif self % 5 == 0
+      'buzz'
+    end
+  end
+  
+end
--- fizzbuzz-alt/spec/fizzbuzz-alt_spec.rb
+++ fizzbuzz-alt/spec/fizzbuzz-alt_spec.rb
@@ -4,4 +4,9 @@ describe 'fizzbuzz' do
   it 'returns "fizz" for multiple of 3' do
     expect(99.fizzbuzz).to eq('fizz')
   end
+  
+  it 'returns "buzz" for multiple of 5' do
+    expect(25.fizzbuzz).to eq('buzz')
+  end
+  
 end
