d17a924986a5df727048ecf5bfb464cb305977b3
mbrad26 <mbrad26@gmail.com>
2020-04-03 16:03:14 +0000

    initial commit

---
+++ stub-mock/lib/stub_mock.rb
@@ -0,0 +1,10 @@
+class DataProcessor
+  Error = Class.new(StandardError)
+  
+  def process data, validator
+    raise Error unless validator.valid?(data)
+    
+    "#{data} processed"
+  end
+  
+end
---
+++ stub-mock/spec/stub_mock_spec.rb
@@ -0,0 +1,16 @@
+require 'stub_mock.rb'
+
+# Stub
+# Returns canned responses, avoiding any meaningful computation or I/O:
+#
+# allow(some_object).to receive(some_method).and_return(some_value)
+# 
+# Mock
+# Expects specific messages; will raise an error if it doesn’t receive them by the end of the example:
+#
+# expect(some_object).to receive(some_method).and_return(some_value)
+
+
+describe DataProcessor do
+  
+end
