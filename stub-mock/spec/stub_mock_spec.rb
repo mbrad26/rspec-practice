560bcf1ddf6c30e6cb8a5b4a4f575fd7df2c3530
mbrad26 <mbrad26@gmail.com>
2020-04-03 16:48:25 +0000

    Added context and another double

--- stub-mock/spec/stub_mock_spec.rb
+++ stub-mock/spec/stub_mock_spec.rb
@@ -13,14 +13,23 @@ require 'spec_helper'
 
 describe DataProcessor do
   
-  let(:processor) { described_class.new }
+  let(:processor) { described_class.new } # let creates an instance only when called for
   
-  it 'adds processed to valid data' do
-    validator = double(:validator) # one line double(:validator, valid?: true)
-    
-    allow(validator).to receive(:valid?).and_return(true)
-    
-    expect(processor.process('foo', validator)).to eq('foo processed')
+  context 'with valid data' do
+    it 'adds processed to valid data' do
+      validator = double(:validator) # one line double(:validator, valid?: true)
+      
+      allow(validator).to receive(:valid?).and_return(true) # stub method
+      
+      expect(processor.process('foo', validator)).to eq('foo processed')
+    end
+  end
+  
+  context 'with invalid data' do
+    it 'raises Error' do
+      validator = double(:validator, valid?: false)
+      expect { processor.process('foo', validator) }.to raise_error(DataProcessor::Error)
+    end
   end
   
 end
