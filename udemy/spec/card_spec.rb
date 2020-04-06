96f4089a5b866a2bd1f2d222160bfd6e7e72e815
mbrad26 <mbrad26@gmail.com>
2020-04-06 14:35:37 +0000

    added tests for rank and suit

--- udemy/spec/card_spec.rb
+++ udemy/spec/card_spec.rb
@@ -1,9 +1,17 @@
 require 'card'
 
 describe Card do
-  it 'has a type method' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.rank).to eq 'Ace'
-    expect(card.suit).to eq 'Spades'
+  
+  before :each do
+    @card = Card.new('Ace', 'Spades')
   end
+  
+  it 'has a rank' do
+    expect(@card.rank).to eq 'Ace'
+  end
+  
+  it 'has a suit' do
+    expect(@card.suit).to eq 'Spades'
+  end
+  
 end
