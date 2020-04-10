ec32861e20ecfdb537ada606c47199db9ba7f2ba
mbrad26 <mbrad26@gmail.com>
2020-04-10 09:44:44 +0000

    refactored equality_matchers_spec.rb

--- udemy/spec/equality_matchers_spec.rb
+++ udemy/spec/equality_matchers_spec.rb
@@ -15,6 +15,9 @@ RSpec.describe 'equality matchers' do
       expect(a).not_to eql 3
       expect(b).not_to eql 3.0
       expect(a).not_to eql b
+      
+      expect(a).to eql 3.0
+      expect(b).to eql 3
     end
   end
 end
