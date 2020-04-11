fa2e90ecc230c6cfced1739b38274524487b187f
mbrad26 <mbrad26@gmail.com>
2020-04-11 11:55:00 +0000

    refactored double_spec.rb

--- udemy/spec/double_spec.rb
+++ udemy/spec/double_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe 'double' do
   it 'only allows defined methods to be invoked' do
-    # stuntman = double('Mr. Danger', fall_off_ladder: 'Ouch', light_on_fire: true)
-    # expect(stuntman.fall_off_ladder).to eq('Ouch')
-    # expect(stuntman.light_on_fire).to eq(true)
+    stuntman = double('Mr. Danger', fall_off_ladder: 'Ouch', light_on_fire: true)
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+    expect(stuntman.light_on_fire).to eq(true)
     
     stuntman = double('Mr. Danger')
     allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
@@ -12,6 +12,6 @@ RSpec.describe 'double' do
     allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true)
     expect(stuntman.fall_off_ladder).to eq 'Ouch'
     expect(stuntman.light_on_fire).to eq true
-    
   end
 end
+
