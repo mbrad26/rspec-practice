994cdeb8c72b806a66fe5d5ff517f3e63abcc8de
mbrad26 <mbrad26@gmail.com>
2020-04-12 15:01:29 +0100

    added tests for Student class methods

--- udemy-tdd/lib/student.rb
+++ udemy-tdd/lib/student.rb
@@ -1,14 +1,14 @@
 class Student
-  
+
   attr_reader :first_name, :last_name
-  
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+
+  def initialize(first, last)
+    @first_name = first
+    @last_name = last
   end
-  
+
   def student_fullname
     "#{first_name} #{last_name}"
   end
-  
+
 end
--- udemy-tdd/spec/student_spec.rb
+++ udemy-tdd/spec/student_spec.rb
@@ -1,18 +1,22 @@
 require 'student'
 
 describe Student do
-  
-  subject { Student.new('Bob', 'Maker') }
-  
-  it 'responds to #first_name' do
-    expect(subject.first_name).to eq 'Bob'
-  end
-  
-  it 'responds to #last_name' do
-    expect(subject.last_name).to eq 'Maker'
-  end
+  describe 'instance methods' do
+    subject { described_class.new('Bob', 'Maker') }
 
-  it 'responds to #student_fullname' do
-    expect(subject.student_fullname).to eq 'Bob Maker'
+    it 'responds to #first_name' do
+      expect(subject).to respond_to(:first_name)
+      # expect(subject.first_name).to eq 'Bob'
+    end
+
+    it 'responds to #last_name' do
+      expect(subject).to respond_to(:last_name)
+      # expect(subject.last_name).to eq 'Maker'
+    end
+
+    it 'responds to #student_fullname' do
+      expect(subject).to respond_to(:student_fullname)
+      # expect(subject.student_fullname).to eq 'Bob Maker'
+    end
   end
 end
