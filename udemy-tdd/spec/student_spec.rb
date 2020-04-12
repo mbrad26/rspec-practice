53fd62eae839225b78e61bc6078ad45cb60edbe9
mbrad26 <mbrad26@gmail.com>
2020-04-12 15:24:47 +0100

    added test for class variable @@total_count

--- udemy-tdd/spec/student_spec.rb
+++ udemy-tdd/spec/student_spec.rb
@@ -1,22 +1,23 @@
 require 'student'
 
 describe Student do
+
   describe 'instance methods' do
     subject { described_class.new('Bob', 'Maker') }
 
-    it 'responds to #first_name' do
-      expect(subject).to respond_to(:first_name)
-      # expect(subject.first_name).to eq 'Bob'
-    end
+    it { respond_to(:first_name) }
 
-    it 'responds to #last_name' do
-      expect(subject).to respond_to(:last_name)
-      # expect(subject.last_name).to eq 'Maker'
-    end
+    it { respond_to(:last_name) }
 
-    it 'responds to #student_fullname' do
-      expect(subject).to respond_to(:student_fullname)
-      # expect(subject.student_fullname).to eq 'Bob Maker'
+    it { respond_to(:student_fullname) }
+  end
+
+  describe 'total number of students created' do
+    it 'should have two students in total' do
+      Student.new('John', 'Doe')
+      Student.new('Jane', 'Doe')
+
+      expect(Student.total_count).to eq 2
     end
   end
 end
