0ed2786dc5c0b84106460ce57361b17e735614eb
mbrad26 <mbrad26@gmail.com>
2020-04-12 19:46:45 +0100

    aded more class methods

--- udemy-tdd/lib/product.rb
+++ udemy-tdd/lib/product.rb
@@ -14,4 +14,20 @@ class Product
   def self.all
     @@products
   end
+
+  def self.product_names
+    # @@products.map { |product| product.name}
+    @@products.map(&:name)
+  end
+
+  def self.products_to_order
+    @@products.select { |product| product.qty == 0 }
+  end
+
+  def self.inventory_value
+    # inventory = 0
+    # @@products.each { |product| inventory += product.qty * product.price }
+    # inventory
+    @@products.reduce(0) { |acc, product| acc + product.qty * product.price}
+  end
 end
--- udemy-tdd/spec/product_spec.rb
+++ udemy-tdd/spec/product_spec.rb
@@ -2,7 +2,7 @@ require 'product'
 
 describe Product do
 
-  before (:all) do
+  before :all do
     @product = described_class.new({id: 1, name: 'Item1', qty: 3, price: 25})
   end
 
@@ -22,7 +22,7 @@ describe Product do
     expect(@product).to respond_to(:price)
   end
 
-    it 'returns correct attributes' do
+   it 'returns correct attributes' do
       expect(@product.id).to eq 1
       expect(@product.name).to eq 'Item1'
       expect(@product.qty).to eq 3
@@ -31,5 +31,19 @@ describe Product do
 
     it 'returns the list of all products' do
       expect(described_class.all).to eq [@product]
+    end
+
+    it 'returns the names of all the products' do
+      expect(described_class.product_names).to eq [@product.name]
+    end
+
+    it 'returns the list of sold out products' do
+      product2 = described_class.new({id: 2, name: 'Item2', qty: 7, price: 15})
+      product3 = described_class.new({id: 3, name: 'Item3', qty: 0, price: 30})
+      expect(described_class.products_to_order).to eq [product3]
+    end
+
+    it 'returns the total inventory class' do
+      expect(described_class.inventory_value).to eq(3 * 25 + 7 * 15 + 0 * 30)
     end
 end
