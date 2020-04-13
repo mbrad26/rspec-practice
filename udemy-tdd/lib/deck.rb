7860e6c105ee0432fbc71761dcca04d79fdcb143
mbrad26 <mbrad26@gmail.com>
2020-04-13 16:00:44 +0100

    added tests for replace_with, deal_card, shuffle

--- udemy-tdd/lib/deck.rb
+++ udemy-tdd/lib/deck.rb
@@ -1,13 +1,51 @@
 require_relative 'card'
 
 class Deck
-  
-  attr_reader :suits, :ranks
-  
+
+  attr_reader :suits, :ranks, :deck
+  SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+  RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
   def initialize(suits, ranks)
     @deck = []
     @suits = suits
     @ranks = ranks
+    create_deck
   end
-  
+
+  def shuffle
+    @deck.shuffle!
+  end
+
+  def deal_card
+    @deck.pop
+  end
+
+  def replace_with(new_deck)
+    @suits = []
+    @ranks = []
+    @deck = new_deck
+
+    new_deck.each do |card|
+      add_suit_and_rank(card)
+    end
+    self
+  end
+
+  private
+
+  def create_deck
+    suits.each do |suit|
+      ranks.each do|rank|
+        @deck << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def add_suit_and_rank(card)
+    suit = card.suit
+    rank = card.rank
+    @suits << suit unless @suits.include? suit
+    @ranks << rank unless @ranks.include? rank
+  end
 end
--- udemy-tdd/spec/deck_spec.rb
+++ udemy-tdd/spec/deck_spec.rb
@@ -3,40 +3,48 @@ require 'deck'
 describe Deck do
   SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
   RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-  
+
   subject(:deck) { described_class.new(SUITS, RANKS) }
-  
+
   it 'responds to "suits"' do
     expect(deck).to respond_to :suits
   end
-  
+
   it 'responds to "ranks"' do
     expect(deck).to respond_to :ranks
   end
-  
-  it 'responds to "shufle"' do
-    expect(deck).to respond_to :shufle
+
+  it 'responds to "shuffle"' do
+    expect(deck).to respond_to :shuffle
   end
-  
+
   it 'responds to "deck"' do
     expect(deck).to respond_to :deck
   end
-  
+
   it 'responds to "deal_card"' do
     expect(deck).to respond_to :deal_card
   end
-  
+
   it 'responds to "replace_with"' do
     expect(deck).to respond_to :replace_with
   end
-  
+
   it 'pops off the stack when a card is dealt' do
     dealt_card = deck.shuffle.last
     expect(deck.deal_card).to eq dealt_card
   end
-  
+
   it 'dealt card cannot be nil' do
     expect(deck.deal_card).not_to eq nil
   end
-  
+
+  it 'gets a new deck with "replace_with"' do
+    deck_of_cards = []
+    deck_of_cards.push(Card.new('Clubs', '2'))
+    deck_of_cards.push(Card.new('Clubs', '3'))
+    new_deck = deck.dup
+    new_deck.replace_with(deck_of_cards)
+    expect(deck.deck).not_to eq new_deck.deck
+  end
 end
